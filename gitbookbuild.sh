3ad1b5130bdf3305cc8c5ea6677b6b0dd2693da1
xugang <xugang@feeyo.com>
2015-07-19 22:53:34 +0800

    repari SUMMARY

--- gitbookbuild.sh
+++ gitbookbuild.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
+read commit
+git add -A
+git commit -m "$commit"
 echo 'build gitbook start...'
 gitbook build
 echo 'copy static html to githuo.io...'
 cp -u -r _book/* /home/vagrant/git/hubeixugang.github.io
 echo 'copy done'
+cd /home/vagrant/git/hubeixugang.github.io
+echo 'in blog'
+git add -A
+git commit -m "$commit"
+git push origin master
+cd /home/vagrant/git/Accumulate
+echo 'push done'
